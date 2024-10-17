73d97d8ad42f3fbdec364d210658000e9287a61e
Drportox <guiportopt@outlook.com>
2024-10-17 22:32:57 +0100

    index's

---
+++ intro/index1.cpp
@@ -0,0 +1,14 @@
+//USANDO COUT 
+#include <iostream> // Includes the standard input-output library, necessary to use 'cout' and 'cin'.
+
+using namespace std; // Allows you to use elements from the C++ standard library without needing to write 'std::' before them.
+
+int main() { // Main function of the program, where execution begins. 'int' indicates that it will return an integer value.
+
+    cout << "Hello world" << endl << "Hello again"; // Sends the message "Hello world" to the screen, followed by a new line, and then "Hello again".
+    // 'cout' is used to display text on the screen.
+    // '<<' is the insertion operator that directs the text to 'cout'.
+    // 'endl' is a manipulator that creates a new line in the console.
+
+    return 0; // Ends the 'main' function and returns 0 to indicate that the program finished successfully.
+}
---
+++ intro/index3.cpp
@@ -0,0 +1,12 @@
+// USING PRINTF FOR C
+#include <cstdio> // Including the C standard library, which is used for the 'printf' function.
+
+int main(void) { // This is the main function of the program; 'int' indicates that the function will return an integer value.
+    
+    printf("Hello\n \t \a world\n"); // This is the string that will be displayed on the screen.
+    // '\n' creates a new line in the output.
+    // '\t' adds a tab space (indentation) in the text.
+    // '\a' produces a beep sound (alert) on the console if supported.
+
+    return 0; // This ends the main function and returns 0, indicating that the program finished successfully.
+}
